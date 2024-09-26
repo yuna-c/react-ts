c35b784fdefb2e667aab798b5a6db71a8b01f32c
yuna <yuna744@gmail.com>
2024-09-27 01:33:29 +0900

    📝 add: Router(useRouter) - SEO 불리 -  onClick 같은 이벤트 핸들러에서 사용 - 클릭 후 로직의 순서에 따라 실행하므로, 즉시 이동X

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client' // useRouter를 사용할 때는 항상 코드 최상단에 “use client”를 삽입
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 // import Image from 'next/image'
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleButtonClick = () => {
+    setTimeout(() => {
+      router.replace('/test')
+    }, 1000)
+  }
+
+  //NOTE - useRouter(히스토리 스택)
+  // router.push 히스토리 스택에 추가
+  // router.replace 현재 URL을 히스토리 스택에서 새로운 URL로 대체, 맨위에 있는 히스토리를 새로운 URL로 대체
+  // router.back 사용자를 히스토리 스택에서 한 단계 뒤로 이동
+  // router.reload 현재 페이지를 새로 고침, 히스토리 스택 영향 X
+
   return (
     <div>
       안녕하세요. next.js임
@@ -10,6 +26,9 @@ export default function Home() {
       </div> */}
       <div>
         <Link href={'/test'}>Link로 test 이동</Link>
+      </div>
+      <div>
+        <button onClick={handleButtonClick}>클릭!</button>
       </div>
     </div>
   )
