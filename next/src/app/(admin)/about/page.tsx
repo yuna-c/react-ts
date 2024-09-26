b08cf6f54426377f21a317989292b568f57b3d75
yuna <yuna744@gmail.com>
2024-09-27 02:35:51 +0900

    📝 add : 특별한 예약 파일들 template -  동일한 Template을 공유하는 경로 사이를 왔다갔다 할 때 DOM 요소가 다시 생성 - 계속 리랜더링

--- next/src/app/(admin)/about/page.tsx
+++ next/src/app/(admin)/about/page.tsx
@@ -1,4 +1,14 @@
+import Link from 'next/link'
+
 export default function AboutPage() {
   // http://localhost:3000/about
-  return <div className="page">소개 페이지야</div>
+  return (
+    <div className="page">
+      소개 페이지야dfsfsdfvds
+      <br />
+      <div>
+        <Link href={'/about2'}>about2로 가</Link>
+      </div>
+    </div>
+  )
 }
