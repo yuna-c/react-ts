3f819062123985c91437ad7a029395ce508c83c0
yuna <yuna744@gmail.com>
2024-09-26 23:34:38 +0900

    📝 add: 폴더 기반 동적(dynamic) 라우팅 - id, parameter 있는 경우

---
+++ next/src/app/post/[id]/page.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  params: {
+    id: number
+  }
+}
+
+// dynamic routing : \post\[id]\page.tsx
+// https://nextjs.org/docs/app/building-your-application/routing#route-segments
+// https://nextjs.org/docs/app/building-your-application/routing#nested-routes
+
+export default function page({ params }: Props) {
+  return (
+    <div className="page">
+      <div>ID : {params.id} Page</div>
+    </div>
+  )
+}
