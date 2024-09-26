c0f02cf6d77528906732e296e4eaebdc1b98b08f
yuna <yuna744@gmail.com>
2024-09-26 12:36:37 +0900

    📝 add: 새로운 Todo 항목 삭제 기능 구현

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -33,10 +33,18 @@ function App() {
     setTitle('')
   }
 
+  //SECTION - 5. delete Todo 항목 삭제 기능 구현
+  const handleDeleteTodo = (id: Todo['id']) => {
+    fetch(`http://localhost:4000/todos/${id}`, {
+      method: 'DELETE'
+    })
+    setTodoList((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
   return (
     <div id="wrap">
       {/* 빈 todoList 배열을 TodoList[] 컴포넌트에 전달 */}
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onDeleteClick={handleDeleteTodo} />
 
       {/* 입력 */}
       {/* <input type="text" onChange={(e) => {}} /> : 가짜 함수 만들어서 호버하면 타입이 나오는거 복붙 */}
@@ -53,21 +61,27 @@ function App() {
 
 //TODO - props: 함수 인자 첫번째에 객체 형태로 타입 지정
 //SECTION - 1. 함수 TodoList의 첫 번째 매개변수인 props에 대해 {객체} 구조로 타입을 지정하여 사용
-type TodoListProps = { todoList: Todo[] }
-function TodoList({ todoList }: TodoListProps) {
+type TodoListProps = {
+  todoList: Todo[]
+  onDeleteClick: (id: Todo['id']) => void
+}
+function TodoList({ todoList, onDeleteClick }: TodoListProps) {
   return (
-    <>
+    <div>
       {todoList.map((todo) => (
         // todoList 배열을 순회하며 각 요소(todo)를 TodoItem 컴포넌트에 props로 전달
-        <TodoItem key={todo.id} {...todo} />
+        <TodoItem key={todo.id} {...todo} onDeleteClick={onDeleteClick} />
       ))}
-    </>
+    </div>
   )
 }
 
 //SECTION - 2. TodoItem 컴포넌트는 todo 객체의 속성들을 받아서 렌더링(직접 그려지는)
-type TodoItemProps = Todo
-function TodoItem({ id, title, completed }: TodoItemProps) {
+type TodoItemProps = Todo & {
+  //TODO - 함수 타입 추가
+  onDeleteClick: (id: Todo['id']) => void
+}
+function TodoItem({ id, title, completed, onDeleteClick }: TodoItemProps) {
   return (
     <div style={{ borderBottom: '1px solid #ddd', marginBottom: '10px', paddingBottom: '10px' }}>
       <div>id: {id}</div>
@@ -76,6 +90,13 @@ function TodoItem({ id, title, completed }: TodoItemProps) {
       {/* todo의 제목을 표시 */}
       <div>completed : {`${completed}`}</div>
       {/* todo의 완료 여부를 표시 boolean이라 표기가 안되어서 string으로 만들어 줘야함 */}
+      <button
+        onClick={() => {
+          onDeleteClick(id)
+        }}
+      >
+        삭제
+      </button>
     </div>
   )
 }
