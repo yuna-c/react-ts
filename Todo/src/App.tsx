6ab6cd2c8ca192b633391ca941e819d3276553cc
yuna <yuna744@gmail.com>
2024-09-26 12:23:01 +0900

    📝 add: 새로운 Todo 항목 추가 기능 구현

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -3,36 +3,56 @@ import { getTodos, type Todo } from './test'
 import './App.css'
 
 function App() {
-  //SECTION - 3. Todo 불러오는 로직
+  //SECTION - 3. 비동기적으로 Todo 불러오는 로직
   const [todoList, setTodoList] = useState<Todo[]>([])
 
   useEffect(() => {
     getTodos().then((data) => setTodoList(data))
   }, [])
 
+  //SECTION - 4. create Todo 항목 추가 기능 구현
   const [title, setTitle] = useState('') // string
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
+    // const target = e.target as HTMLInputElement // 타입캐스팅
     console.log(e.target.value)
     setTitle(e.target.value) // 세터 함수
     console.log(title)
   }
 
+  const handleAddTodo = async () => {
+    if (title === '') return
+    const newTodo: Todo = { id: crypto.randomUUID(), title: title, completed: false }
+    await fetch('http://localhost:4000/todos', {
+      method: 'POST',
+      body: JSON.stringify(newTodo)
+    })
+
+    setTodoList((prev) => [...prev, newTodo])
+    setTitle('')
+  }
+
   return (
     <div id="wrap">
-      {/* 빈 todoList 배열을 TodoList 컴포넌트에 전달 */}
+      {/* 빈 todoList 배열을 TodoList[] 컴포넌트에 전달 */}
       <TodoList todoList={todoList} />
 
       {/* 입력 */}
-      {/* <input type="text" onChange={(e) => {}} /> : 가짜 함수 만들어서 호버하면 타입이 나오는거 복붙*/}
-      <input type="text" value={title} onChange={handleTitleChange} />
+      {/* <input type="text" onChange={(e) => {}} /> : 가짜 함수 만들어서 호버하면 타입이 나오는거 복붙 */}
+
+      <input
+        type="text"
+        value={title} // 기본값 설정으로 undefined 방지
+        onChange={handleTitleChange}
+      />
+      <button onClick={handleAddTodo}>등록</button>
     </div>
   )
 }
 
 //TODO - props: 함수 인자 첫번째에 객체 형태로 타입 지정
-//SECTION - 1. 함수 TodoList의 첫 번째 매개변수인 props에 대해 객체 구조로 타입을 지정하여 사용
+//SECTION - 1. 함수 TodoList의 첫 번째 매개변수인 props에 대해 {객체} 구조로 타입을 지정하여 사용
 type TodoListProps = { todoList: Todo[] }
 function TodoList({ todoList }: TodoListProps) {
   return (
@@ -49,13 +69,13 @@ function TodoList({ todoList }: TodoListProps) {
 type TodoItemProps = Todo
 function TodoItem({ id, title, completed }: TodoItemProps) {
   return (
-    <div>
+    <div style={{ borderBottom: '1px solid #ddd', marginBottom: '10px', paddingBottom: '10px' }}>
       <div>id: {id}</div>
       {/* todo의 id를 표시 */}
       <div>title: {title}</div>
       {/* todo의 제목을 표시 */}
       <div>completed : {`${completed}`}</div>
-      {/* todo의 완료 여부를 표시 */}
+      {/* todo의 완료 여부를 표시 boolean이라 표기가 안되어서 string으로 만들어 줘야함 */}
     </div>
   )
 }
