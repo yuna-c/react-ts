9ed09d4f4e4e88ab750a915d49e95ffc19981351
yuna <yuna744@gmail.com>
2024-09-27 01:08:11 +0900

    📝add:  페이지 이동과 관련된 기능 목록 - Link - prefetching - route 사이에 client-side navigation을 지원 - 딜레이 없이 넘어감

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link'
 // import Image from 'next/image'
 
 export default function Home() {
-  return <div>안녕하세요. next.js임</div>
+  return (
+    <div>
+      안녕하세요. next.js임
+      {/* <div>
+        <a href={'/test'}>a로 test 이동</a>
+      </div> */}
+      <div>
+        <Link href={'/test'}>Link로 test 이동</Link>
+      </div>
+    </div>
+  )
 }
