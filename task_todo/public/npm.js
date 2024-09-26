424119904212ba51eed9faf1a6f366e5798df4ab
yuna <yuna744@gmail.com>
2024-09-26 14:33:30 +0900

    📝 add: task setting

---
+++ task_todo/public/npm.js
@@ -0,0 +1,10 @@
+// npm create vite@latest my-react-ts-app -- --template react-ts
+// yarn
+// npm run dev
+
+// yarn add json-server
+// json-server --watch db.json --port 4000
+// yarn server
+// https://www.npmjs.com/package/json-server
+
+// node : ts 컴파일러 => npx tsc test.ts
