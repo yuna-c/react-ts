9889cdff98ad312fa9bc9cf1b3f740d2e3dec551
yuna <yuna744@gmail.com>
2024-09-30 20:40:12 +0900

    📝 add: Client Component - useState, useEffect 훅은 client component

--- rendering/src/app/page.tsx
+++ rendering/src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client' // 클라이언트 컴포넌트
+import { useEffect } from 'react'
+
 export default function Home() {
   // 환경
-  const os = require('os')
-  console.log(os.hostname())
+  // const os = require('os')
+  // console.log(os.hostname())
 
   // 서버 컴포넌트
-  console.log('너 혹시 서버 컴포넌트야? 어디 있는데?')
+  // console.log('너 혹시 서버 컴포넌트야? 어디 있는데?')
+
+  // 클라이언트 컴포넌트(브라우저)
+  console.log('너 혹시 클라이언트 컴포넌트야? 어디 있는데?')
+  useEffect(() => {
+    console.log('서버에서 완전 빠졌지?')
+  }, [])
 
   return <div className="p-8 m-4">안녕하세요. next.js 입니다</div>
 }
