7f1e5f6b460f7e782c267b8c8767890d06c5e49e
yuna <yuna744@gmail.com>
2024-10-01 01:43:44 +0900

    📝 add: custom errorsms client - 오류 메세지 출력 - 리액트 에러 바운드리의 클래스 컴포넌트에서만 제공하는 라이프사이클 메서드를 활용하기 위해 error boundary라는 class 컴포넌트 사용 - 트리 안에서 자식요소가 깨졌을 때 fallbackUI를 보여주는 형태 - 가까운 상위 에러 바운더리로 오류 토스

--- rendering/src/app/_components/NewProductList.tsx
+++ rendering/src/app/_components/NewProductList.tsx
@@ -1,13 +1,15 @@
 import { Product } from '@/types/product'
 
 const NewProductList = async () => {
-  const res = await fetch('http://localhost:4001/products', {
+  const res = await fetch('http://localhost:4000/products', {
     cache: 'no-store'
   })
   const data: Product[] = await res.json()
   const newData = data.filter((p) => p.isNew)
   // Product 타입의 필드 중 하나로, 각 제품이 "새 제품"인지 여부를 나타내는 불리언(boolean) 값
   console.log(newData)
+
+  throw new Error('오류!')
 
   return (
     <div className="flex gap-2 oveflow-auto">
