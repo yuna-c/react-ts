565f9ad8be8dd4de92f6e3238fac50dd5bfa84dc
yuna <yuna744@gmail.com>
2024-09-30 22:19:29 +0900

    📝 add: SSG (Static Site Generation) - 기본값일 때 정적 데이터 가지고 오기 - yarn build && yarn start

--- rendering/src/app/(marketing)/business/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function MarketingBusinessPage() {
-  return <div className="page">사업 관련 페이지</div>
-}
--- rendering/src/app/page.tsx
+++ rendering/src/app/page.tsx
@@ -1,17 +1,50 @@
-import ClientExample from './components/ClientExample'
+// https://fakestoreapi.com/
+// SSG : 빌드타임에 홈페이지 만들어 줌(정적)
 
-export default function Home() {
-  // 환경
-  const os = require('os')
-  console.log(os.hostname())
-
-  // 서버 컴포넌트
-  console.log('안녕 나는 서버 컴포넌트야❤')
+export default async function Home() {
+  const res = await fetch('http://localhost:4000/products')
+  const data: Product[] = await res.json()
+  console.log(`data`, data)
 
   return (
     <div className="p-8 m-4">
-      안녕하세요. next.js 입니다
-      <ClientExample />
+      {data.map((product) => (
+        <div className="flex border p-4 gap-4 rounded-md" key={product.id}>
+          <img className="rounded-smr" width={150} height={150} src={product.images} alt={product.title} />
+          <div className="flex flex-col justify-between">
+            <div>
+              <h2 className="text-xl font-bold">{product.title}</h2>
+              <p className="text-sm">{product.description}</p>
+              <p className="mt-4 text-2xl">{product.price.amount}$</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
+}
+
+export type Product = {
+  id: number
+  handle: string
+  availableForSale: boolean
+  isNew: boolean
+  title: string
+  description: string
+  descriptionHtml: string
+  options: {
+    name: string
+    values: string[]
+  }[]
+  price: {
+    amount: string
+    currencyCode: string
+  }
+  images: string
+  seo: {
+    title: string
+    description: string
+  }
+  tags: string[]
+  rating: number
 }
