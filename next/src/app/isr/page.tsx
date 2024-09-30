4b06fa17c6173ccee5bdb6e0e1cf0941ca2c52f6
yuna <yuna744@gmail.com>
2024-10-01 03:18:44 +0900

    📝 add: ISR(Incremental Static Regeneration)

--- next/src/app/isr/page.tsx
+++ next/src/app/isr/page.tsx
@@ -1,3 +1,23 @@
+import { Post } from '@/types/Post'
+
 export default async function ISRPage() {
-  return <div>page</div>
+  const res = await fetch('http://localhost:4000/posts', {
+    next: {
+      revalidate: 5
+    }
+  })
+  const data: Post[] = await res.json()
+  console.log(data)
+
+  return (
+    <div>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h1>{post.id}</h1>
+          <h2>{post.title}</h2>
+          <p>{post.author}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
