57700e63dc16ca91073416054bd5458060484c78
yuna <yuna744@gmail.com>
2024-09-30 22:59:23 +0900

    📝 add: ISR(Incremental Static Regeneration) - revalidate 옵션으로 초단위로 설정되며 페이지 처음 요청 후 다음 재 생성을 언제 할 지 결정

--- rendering/src/app/page.tsx
+++ rendering/src/app/page.tsx
@@ -1,10 +1,33 @@
 // https://fakestoreapi.com/
-// CSR : 클라이언트 컴포넌트를 만들어서 use client를 써서 사용, 서버가 아니라 클라이언트 콘솔에서 렌더가 뜬다
-
-import ProductList from './_components/ProductList'
+// ISR : 정적 증분 방식, 특정 타입에만 랜더링이 다시 되는 SSG,SSR 섞인 방식
+// 특정 시간이 지나고 나서 랜더링 실행
 
 export default async function Home() {
-  return <ProductList />
+  const res = await fetch('http://localhost:4000/products', {
+    // cache: '',
+    next: {
+      revalidate: 3
+    }
+  })
+  const data: Product[] = await res.json()
+  console.log(`render:: render`)
+
+  return (
+    <div className="p-8 m-4">
+      {data.map((product) => (
+        <div className="flex border p-4 gap-4 rounded-md" key={product.id}>
+          <img className="rounded-smr" width={150} height={150} src={product.images} alt={product.title} />
+          <div className="flex flex-col justify-between">
+            <div>
+              <h2 className="text-xl font-bold">{product.title}</h2>
+              <p className="text-sm">{product.description}</p>
+              <p className="mt-4 text-2xl">{product.price.amount}$</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export type Product = {
