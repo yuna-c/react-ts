a1e2342fe0d260d27702baebce03ceb188d014c0
yuna <yuna744@gmail.com>
2024-09-30 22:52:23 +0900

    📝 add: CSR(Client Side Rendering) - useState, useEffect는 무조건 클라이언트에서 동작 해야함

--- rendering/src/app/page.tsx
+++ rendering/src/app/page.tsx
@@ -1,31 +1,10 @@
 // https://fakestoreapi.com/
-// SSR : 사용자가 페이지에 접속 시도(요청) 할때 마다 새로 컨텐츠 불러와서 보여주는
-// 패치안에서 캐싱이 되지 않으니까 매번 새로고침 할 때마다 랜더링
+// CSR : 클라이언트 컴포넌트를 만들어서 use client를 써서 사용, 서버가 아니라 클라이언트 콘솔에서 렌더가 뜬다
+
+import ProductList from './_components/ProductList'
 
 export default async function Home() {
-  const res = await fetch('http://localhost:4000/products', {
-    cache: 'no-store'
-    // cache: 'force-cache' => SSG변경(안쓰면 기본 값으로 force-cache)
-  })
-  const data: Product[] = await res.json()
-  console.log(`render`)
-
-  return (
-    <div className="p-8 m-4">
-      {data.map((product) => (
-        <div className="flex border p-4 gap-4 rounded-md" key={product.id}>
-          <img className="rounded-smr" width={150} height={150} src={product.images} alt={product.title} />
-          <div className="flex flex-col justify-between">
-            <div>
-              <h2 className="text-xl font-bold">{product.title}</h2>
-              <p className="text-sm">{product.description}</p>
-              <p className="mt-4 text-2xl">{product.price.amount}$</p>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
+  return <ProductList />
 }
 
 export type Product = {
