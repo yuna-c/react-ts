1f0c50d507a03ca093f52585ed43acc9c58a1c05
yuna <yuna744@gmail.com>
2024-10-01 01:35:02 +0900

    📝 add: 레이아웃과 마찬가지로 특정 세그먼트 안에서 작동하는 로딩 스크린

---
+++ rendering/src/app/product/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Loading = () => {
+  return <div>Product Loading중이양</div>
+}
+
+export default Loading
---
+++ rendering/src/app/product/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductList from '../_components/ProductList'
+
+const TestPage = () => {
+  return (
+    <div>
+      TestPage
+      <ProductList />
+    </div>
+  )
+}
+
+export default TestPage
