4e50e116bfbdbceca0fb10b556e828a6f056805a
yuna <yuna744@gmail.com>
2024-10-02 00:31:11 +0900

    📝 add: cart에 장바구니 숫자 띄우기 로직 zustand

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Logo from '/public/assets/logo.png'
 import Link from 'next/link'
+import { CartProvider } from '@/components/providers/ZustandProvider'
+import CartButton from '@/components/_/CartButton'
 
 export default function Layout({
   children,
@@ -10,15 +12,14 @@ export default function Layout({
   modal: React.ReactNode
 }>) {
   return (
-    <>
+    // 하단 부분 전역 상태 관리 가능
+    <CartProvider products={[]}>
       <header className="relative flex h-[60px] justify-center items-center">
         <Link href={'/'}>
           <Image height={40} src={Logo} alt="logo"></Image>
         </Link>
         <div className="absolute flex gap-2 right-[40px] top-1/2 -translate-y-1/2">
-          <Link href={'/cart'}>
-            <button className="bg-gray-800 text-white px-4 py-2 rounded-md">Cart</button>
-          </Link>
+          <CartButton />
           <Link href={'/sign-in'}>
             <button className="bg-gray-800 text-white px-4 py-2 rounded-md">Sign In</button>
           </Link>
@@ -26,6 +27,6 @@ export default function Layout({
       </header>
       {modal}
       {children}
-    </>
+    </CartProvider>
   )
 }
---
+++ src/components/_/CartButton.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Link from 'next/link'
+import { useCartContext } from '../providers/ZustandProvider'
+
+const CartButton = () => {
+  const products = useCartContext((state) => state.products)
+
+  return (
+    <Link href={'/cart'}>
+      <button className="bg-gray-800 text-white px-4 py-2 rounded-md">
+        Cart
+        <span>{products.length}</span>
+      </button>
+    </Link>
+  )
+}
+
+export default CartButton
