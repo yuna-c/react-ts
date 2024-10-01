972604e466da611f75e5b6030783aca2984bb1bf
yuna <yuna744@gmail.com>
2024-10-02 01:03:12 +0900

    📝 add: 카트 컨텍스트 전역 관리로 구매 목록 활성화

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -1,40 +1,30 @@
-import { getProducts } from "@/server-action";
-import { Product } from "@/type/product";
-import Link from "next/link";
-import Image from "next/image";
+'use client'
 
-const CartPage = async () => {
-  const { data: products }: { data: Product[] } = await getProducts();
+import Link from 'next/link'
+import Image from 'next/image'
+import { useCartContext } from '@/components/providers/ZustandProvider'
+
+const CartPage = () => {
+  // const { data: products }: { data: Product[] } = await getProducts()
+  const { products } = useCartContext((state) => state)
+
   return (
     <section className="flex flex-col items-center p-5 w-full justify-center max-w-screen-lg m-auto min-h-screen py-2">
       <h1>Cart</h1>
       {products.length > 0 ? (
         <div className="flex flex-col gap-4 w-full">
           {products.map((product) => (
-            <div
-              key={product.id}
-              className="flex items-center gap-4 pb-4 border-b border-white"
-            >
-              <Image
-                width={80}
-                height={80}
-                src={product.images}
-                alt={product.title}
-                className="w-[80px] h-[80px] object-cover flex-shrink-0"
-              ></Image>
+            <div key={product.id} className="flex items-center gap-4 pb-4 border-b border-white">
+              <Image width={80} height={80} src={product.images} alt={product.title} className="w-[80px] h-[80px] object-cover flex-shrink-0"></Image>
               <div className="flex-1 truncate">
                 {product.title} - ${product.price.amount}
               </div>
-              <div>{0}</div>
-              <button className="bg-gray-800 text-white px-4 py-2 rounded-md">
-                Remove
-              </button>
+              <div>{product.quantity}</div>
+              <button className="bg-gray-800 text-white px-4 py-2 rounded-md">Remove</button>
             </div>
           ))}
 
-          <button className="bg-gray-800 text-white px-4 py-2 rounded-md">
-            Checkout
-          </button>
+          <button className="bg-gray-800 text-white px-4 py-2 rounded-md">Checkout</button>
         </div>
       ) : (
         <div>
@@ -42,7 +32,7 @@ const CartPage = async () => {
         </div>
       )}
     </section>
-  );
-};
+  )
+}
 
-export default CartPage;
+export default CartPage
